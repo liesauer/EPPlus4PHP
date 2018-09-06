ca1ede14f6af823e5bfc1f662cd49f148941a53c
LiesAuer <liesauer@liesauer.net>
2018-09-06 10:09:59 +0800

    test for cell addressing

--- EPPlus4PHP.Test.PHP/program.php
+++ EPPlus4PHP.Test.PHP/program.php
@@ -1,19 +1,28 @@
 <?php
 
 use nulastudio\Document\EPPlus4PHP\ExcelPackage;
+use nulastudio\Document\EPPlus4PHP\Range;
 
-$package = new ExcelPackage(__DIR__ . '/test.xlsx');
+// $package = new ExcelPackage(__DIR__ . '/test.xlsx');
 
 
 // worksheet
-var_dump(isset($package->workBook->workSheets[1]));
-var_dump(isset($package->workBook->workSheets[2]));
-var_dump(isset($package->workBook->workSheets[3]));
-var_dump(isset($package->workBook->workSheets['test sheet']));
-var_dump(isset($package->workBook->workSheets['test sheet2']));
-var_dump($package->workBook->workSheets['test sheet2']->cells['A1']);
+// var_dump(isset($package->workBook->workSheets[1]));
+// var_dump(isset($package->workBook->workSheets[2]));
+// var_dump(isset($package->workBook->workSheets[3]));
+// var_dump(isset($package->workBook->workSheets['test sheet']));
+// var_dump(isset($package->workBook->workSheets['test sheet2']));
+// var_dump($package->workBook->workSheets['test sheet2']->cells['A1']);
 
 // $package->workBook->workSheets->add('test sheet');
+
+// addressing
+var_dump(Range::parseAddress("a"));
+var_dump(Range::parseAddress("1"));
+var_dump(Range::parseAddress("a:a"));
+var_dump(Range::parseAddress("da"));
+var_dump(Range::parseAddress("1:1"));
+var_dump(Range::parseAddress("a,1,1:1,a2,a2:a5"));
 
 // $package->save();
 
@@ -38,11 +47,11 @@ var_dump($package->workBook->workSheets['test sheet2']->cells['A1']);
 
 // addressing and cells
 
-$range = $package->workBook->workSheets['test sheet']->cells['A1']->style->font->size = 50;
+// $range = $package->workBook->workSheets['test sheet']->cells['A1']->style->font->size = 50;
 
 // var_dump($range);
 
 // $package->workBook->workSheets['test sheet']->cells['A1']->font->size = 50;
 
 
-$package->save();
+// $package->save();
