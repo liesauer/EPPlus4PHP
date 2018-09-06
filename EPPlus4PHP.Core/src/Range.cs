38f22c453143cf63724bdb1fa27a126ab729e98a
LiesAuer <liesauer@liesauer.net>
2018-09-06 09:50:27 +0800

    Cell Addressing

--- EPPlus4PHP.Core/src/Range.cs
+++ EPPlus4PHP.Core/src/Range.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Pchp.Core;
 using Pchp.Library;
 using OfficeOpenXml;
+using System.Text.RegularExpressions;
 
 namespace nulastudio.Document.EPPlus4PHP
 {
@@ -50,6 +53,56 @@ namespace nulastudio.Document.EPPlus4PHP
         {
             return false;
         }
+        #endregion
+
+        #region Addressing
+        // 单选 ["B1"]
+        // 列选 ["A"] ["A:A"]
+        // 行选 [1]["1"]["1:1"]
+        // 窗选 ["A1:B2"]
+        // 多选 ["A1:B2,A,8"]["A1:B2,A:A,1:1000"]
+        public static string parseAddress(Context ctx, PhpValue address)
+        {
+            return parseAddress(address.ToString(ctx));
+        }
+        public static string parseAddress(Context ctx, PhpString address)
+        {
+            return parseAddress(address.ToString(ctx));
+        }
+        public static string parseAddress(string address)
+        {
+            #warning invalid address or out-of-bounds address cannot be detected till now.
+            string normalizeAddress(string _address)
+            {
+                int row = 0;
+                if (int.TryParse(_address, out row))
+                {
+                    // row
+                    return $"{row}:{row}";
+                }
+                else
+                {
+                    // pure column
+                    if (Regex.IsMatch(_address, "^[A-Z]+$"))
+                    {
+                        return $"{_address}:{_address}";
+                    }
+                    else
+                    {
+                        return _address;
+                    }
+                }
+            }
+            address = Regex.Replace(address, "[^0-9a-zA-Z\\:\\,]", "").ToUpper();
+            string[] addresses = address.Split(',');
+            List<string> res = new List<string>();
+            foreach (string addr in addresses)
+            {
+                res.Add(normalizeAddress(addr));
+            }
+            return string.Join(",",res);
+        }
+
         #endregion
 
 
