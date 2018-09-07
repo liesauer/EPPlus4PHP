648c561a6fd3c3ee0c4578c0f9d1a7e0e022f641
LiesAuer <liesauer@liesauer.net>
2018-09-07 22:01:50 +0800

    test

--- EPPlus4PHP.Test.PHP/program.php
+++ EPPlus4PHP.Test.PHP/program.php
@@ -4,6 +4,8 @@ use nulastudio\Document\EPPlus4PHP\ExcelPackage;
 use nulastudio\Document\EPPlus4PHP\ExcelConvert;
 use nulastudio\Document\EPPlus4PHP\Range;
 use nulastudio\Document\EPPlus4PHP\Style\Color;
+use nulastudio\Document\EPPlus4PHP\Style\UnderLineType;
+use nulastudio\Document\EPPlus4PHP\Style\VerticalAlignmentFont;
 
 $package = new ExcelPackage(__DIR__ . '/test.xlsx');
 
@@ -39,12 +41,27 @@ $package = new ExcelPackage(__DIR__ . '/test.xlsx');
 // var_dump(Range::tryParseAddress("a,1,1:1,a2,88888888,1q:q1,a2:a5", $tmp_addr), $tmp_addr); // n
 
 // style font color
-$package->workBook->workSheets['test sheet']->cells['A1']->style->font->color = 0x00BBCCDD;
-$color = $package->workBook->workSheets['test sheet']->cells['A1']->style->font->color;
-// var_dump($color);
-$package->workBook->workSheets['test sheet']->cells['A1']->style->font->size = 50;
+// $package->workBook->workSheets['test sheet']->cells['A1']->style->font->color = 0x00BBCCDD;
+// $color = $package->workBook->workSheets['test sheet']->cells['A1']->style->font->color;
+// // var_dump($color);
+// $package->workBook->workSheets['test sheet']->cells['A1']->style->font->size = 50;
 
-$color->alpha = 0xBB;
+// $color->alpha = 0xBB;
+
+// style font underLineType
+// $package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLine = true;
+// $package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLineType = UnderLineType::Double;
+// var_dump($package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLineType);
+// $package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLine = false;
+// var_dump($package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLineType);
+// $package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLineType = UnderLineType::SingleAccounting;
+// var_dump($package->workBook->workSheets['test sheet']->cells['A1']->style->font->underLineType);
+
+// style font verticalAlignmentFont
+// bug: effective only for the first letter
+$package->workBook->workSheets['test sheet']->cells['A1']->style->font->verticalAlign = VerticalAlignmentFont::Superscript;
+var_dump($package->workBook->workSheets['test sheet']->cells['A1']->style->font->verticalAlign);
+
 
 $package->save();
 
