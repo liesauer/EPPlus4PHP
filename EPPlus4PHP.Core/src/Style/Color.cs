6c80d8ccfa78633183e0acc228607d102d418a9b
LiesAuer <liesauer@liesauer.net>
2018-09-07 12:37:27 +0800

    add 15 built-in colors

--- EPPlus4PHP.Core/src/Style/Color.cs
+++ EPPlus4PHP.Core/src/Style/Color.cs
@@ -10,14 +10,26 @@ namespace nulastudio.Document.EPPlus4PHP.Style
 {
     public class Color
     {
-        public const long RGB = 0xFF0000;
-        public const long GREEN = 0x00FF00;
-        public const long BLUE = 0x0000FF;
+        public const long BLACK_COLOR = 0xFF000000;      // 0.0 white
+        public const long DARKGRAY_COLOR = 0xFF555555;   // 0.333 white
+        public const long LIGHTGRAY_COLOR = 0xFFAAAAAA;  // 0.667 white
+        public const long WHITE_COLOR = 0xFFFFFFFF;      // 1.0 white
+        public const long GRAY_COLOR = 0xFF7F7F7F;       // 0.5 white
+        public const long RED_COLOR = 0xFFFF0000;        // 1.0, 0.0, 0.0 RGB
+        public const long GREEN_COLOR = 0xFF00FF00;      // 0.0, 1.0, 0.0 RGB
+        public const long BLUE_COLOR = 0xFF0000FF;       // 0.0, 0.0, 1.0 RGB
+        public const long CYAN_COLOR = 0xFF00FFFF;       // 0.0, 1.0, 1.0 RGB
+        public const long YELLOW_COLOR = 0xFFFFFF00;     // 1.0, 1.0, 0.0 RGB
+        public const long MAGENTA_COLOR = 0xFFFF00FF;    // 1.0, 0.0, 1.0 RGB
+        public const long ORANGE_COLOR = 0xFF7F007F;     // 1.0, 0.5, 0.0 RGB
+        public const long PURPLE_COLOR = 0xFF7F007F;     // 0.5, 0.0, 0.5 RGB
+        public const long BROWN_COLOR = 0xFF996633;      // 0.6, 0.4, 0.2 RGB
+        public const long CLEAR_COLOR = 0x00000000;      // 0.0 white, 0.0 alpha
         private int _alpha;
         private int _red;
         private int _green;
         private int _blue;
-        
+
         public Color() : this(0, 255, 255, 255)
         {
         }
