da3638f56b61e6bcbce583c567df741ac9257443
LiesAuer <liesauer@liesauer.net>
2018-09-11 20:40:21 +0800

    fix ValueChangedEventArgs property getter bug

--- EPPlus4PHP.Core/src/KVO/ValueChangedEventArgs.cs
+++ EPPlus4PHP.Core/src/KVO/ValueChangedEventArgs.cs
@@ -3,11 +3,11 @@ namespace nulastudio.KVO
     public class ValueChangedEventArgs
     {
         private string _propertyName;
-        public string PropertyName { get; }
+        public string PropertyName { get => _propertyName; }
         private object _oldValue;
-        public object OldValue { get; }
+        public object OldValue { get => _oldValue; }
         private object _newValue;
-        public object NewValue { get; }
+        public object NewValue { get => _newValue; }
 
         public ValueChangedEventArgs(string propertyName, object oldValue, object newValue)
         {
